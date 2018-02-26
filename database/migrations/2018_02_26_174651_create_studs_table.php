0155489dcf5c2c788b3dd375020c97abc85a9be4
pmrvic <mrvic.predrag@gmail.com>
2018-02-26 20:09:27 +0100

    dodan faker za stud model

--- database/migrations/2018_02_26_174651_create_studs_table.php
+++ database/migrations/2018_02_26_174651_create_studs_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Faker\Generator as Faker;
 
 class CreateStudsTable extends Migration
 { 
@@ -26,8 +27,8 @@ class CreateStudsTable extends Migration
                 ->unsigned()
                 ->nullable()
                 ->index('pbrstan');  
-             $table->dateTime('datrodstud')->nullable();;
-             $table->char('jmbgstud',13)->nullable();;
+             $table->dateTime('datrodstud')->nullable();
+             $table->char('jmbgstud',13)->nullable();
             $table->timestamps();
         });
     }

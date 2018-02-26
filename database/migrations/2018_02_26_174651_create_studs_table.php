f249f50b6f579c3e5ce53aba551cd3403d97d6e3
pmrvic <mrvic.predrag@gmail.com>
2018-02-26 19:39:04 +0100

    dodan faker za stud model

---
+++ database/migrations/2018_02_26_174651_create_studs_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudsTable extends Migration
+{ 
+  /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+     $faker = Faker\Factory::create();
+        Schema::create('studs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('imestud',40);
+            $table->char('prezstud',40);
+             $table->integer('pbrrod')
+                ->unsigned()
+                ->nullable()
+                ->index('pbrrod');
+             $table->integer('pbrstan')
+                ->unsigned()
+                ->nullable()
+                ->index('pbrstan');  
+             $table->dateTime('datrodstud')->nullable();;
+             $table->char('jmbgstud',13)->nullable();;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('studs');
+    }
+}
